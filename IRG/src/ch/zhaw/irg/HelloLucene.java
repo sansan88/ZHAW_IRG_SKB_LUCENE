574802e298b433dd1c0271907bd6c16dc7dc9a15
Sandro Scalco <scalpo@bluewin.ch>
2013-04-18 00:19:34 +0200

    first commit

---
+++ IRG/src/ch/zhaw/irg/HelloLucene.java
@@ -0,0 +1,151 @@
+package ch.zhaw.irg;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class HelloLucene {
+	public static void main(String[] args) throws IOException, ParseException {
+		// 0. Specify the analyzer for tokenizing text.
+		// The same analyzer should be used for indexing and searching
+		StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
+
+		// 1. create the index
+		Directory index = new RAMDirectory();
+
+		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40,
+				analyzer);
+
+		// READ COLLECTION
+		try {
+
+//			<?xml version="1.0"?>
+//			<company>
+//				<staff id="1001">
+//					<firstname>yong</firstname>
+//					<lastname>mook kim</lastname>
+//					<nickname>mkyong</nickname>
+//					<salary>100000</salary>
+//				</staff>
+//				<staff id="2001">
+//					<firstname>low</firstname>
+//					<lastname>yin fong</lastname>
+//					<nickname>fong fong</nickname>
+//					<salary>200000</salary>
+//				</staff>
+//			</company>
+			
+			File fXmlFileEN = new File(
+					"C:\\Users\\sandro\\Dropbox\\ZHAW\\63_IRG\\SKB\\irg_collection.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			org.w3c.dom.Document doc = dBuilder.parse(fXmlFileEN);
+
+			//optional, but recommended
+			//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+			doc.getDocumentElement().normalize();
+		 
+			//ROOT --> TREC
+			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+		 
+			NodeList nList = doc.getElementsByTagName("DOC");
+		 
+			System.out.println("----------------------------");
+			
+			System.out.println(" Create Index Writer ");
+			IndexWriter w = new IndexWriter(index, config);
+			
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+		 
+				Node nNode = nList.item(temp);
+		 
+				System.out.println("\nCurrent Element :" + nNode.getNodeName());
+		 
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+		 
+					Element eElement = (Element) nNode;
+		 
+					//System.out.println("Staff id : " + eElement.getAttribute("id"));
+					System.out.println("recordId : " + eElement.getElementsByTagName("recordId").item(0).getTextContent());
+					System.out.println("text : " 	 + eElement.getElementsByTagName("text").item(0).getTextContent());
+					
+					//add to collection
+					addDoc(w, eElement.getElementsByTagName("text").item(0).getTextContent(), eElement.getElementsByTagName("recordId").item(0).getTextContent());
+					
+				}
+			}
+			//close doc
+			w.close();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		// 2. query
+		String querystr = args.length > 0 ? args[0] : "lucene";
+
+		// the "title" arg specifies the default field to use
+		// when no field is explicitly specified in the query.
+		Query q = new QueryParser(Version.LUCENE_40, "title", analyzer)
+				.parse(querystr);
+
+		// 3. search
+		int hitsPerPage = 10;
+		IndexReader reader = DirectoryReader.open(index);
+		IndexSearcher searcher = new IndexSearcher(reader);
+		TopScoreDocCollector collector = TopScoreDocCollector.create(
+				hitsPerPage, true);
+		searcher.search(q, collector);
+		ScoreDoc[] hits = collector.topDocs().scoreDocs;
+
+		// 4. display results
+		System.out.println("Found " + hits.length + " hits.");
+		for (int i = 0; i < hits.length; ++i) {
+			int docId = hits[i].doc;
+			Document d = searcher.doc(docId);
+			System.out.println((i + 1) + ". " + d.get("isbn") + "\t"
+					+ d.get("title"));
+		}
+
+		// reader can only be closed when there
+		// is no need to access the documents any more.
+		reader.close();
+	}
+
+	private static void addDoc(IndexWriter w, String text, String recordId)
+			throws IOException {
+		Document doc = new Document();
+		doc.add(new TextField("text", text, Field.Store.YES));
+		
+		// use a string field for isbn because we don't want it tokenized
+		doc.add(new StringField("recordId", recordId, Field.Store.YES));
+		w.addDocument(doc);
+	}
+}
