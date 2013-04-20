948375dff6a52bb05421eef9e91bb91c4878633f
Sandro Scalco <scalpo@bluewin.ch>
2013-04-20 14:22:54 +0200

    GUI, aufteilung versucht,

--- IRG/src/ch/zhaw/irg/MyFrame.java
+++ IRG/src/ch/zhaw/irg/MyFrame.java
@@ -6,83 +6,96 @@ import java.awt.Color;
 import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.Panel;
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-public class MyFrame extends Frame implements WindowListener, ActionListener{
+public class MyFrame extends Frame implements WindowListener, ActionListener {
 	private Color bg;
-	
-	//Gui Elemente
+
+	// Gui Elemente
 	private Panel panelTop;
+	private Panel panelCenter;
 	private Panel panelLeft;
-	private Panel panelRight; 
-	
+	private Panel panelRight;
+
 	private Button btnSearch = null;
 	private Button btnExportLeft = null;
-	private Button btnExportRight = null; 
-	
-	
-	public static void main(String args[]){
+	private Button btnExportRight = null;
+
+	private TextArea txtAreaLeft;
+	private TextArea txtAreaRight;
+
+	public static void main(String args[]) {
 		MyFrame myFrame = new MyFrame();
 		myFrame.setSize(600, 600);
 		myFrame.setVisible(true);
 		myFrame.setLayout(new BorderLayout());
 	}
-	
-	public MyFrame(){
+
+	public MyFrame() {
 		this.addWindowListener(this);
-		
-		bg = new Color(255,255,200);
+
+		bg = new Color(255, 255, 200);
 		setBackground(bg);
-		
+
 		setTitle("IRG Search by scalcsan & mamutnad");
-		
-		//GUI Komponenten erzeugen
-		
-		//TOP
+
+		// GUI Komponenten erzeugen
+
+		// TOP
 		panelTop = new Panel();
-		panelTop.setLayout(new GridLayout(2,1));
-		
+		panelTop.setLayout(new GridLayout(2, 1));
+
 		btnSearch = new Button("GoGoGo");
 		panelTop.add(btnSearch);
-		
+
 		add(panelTop, BorderLayout.NORTH);
-		
-		//LEFT
-		panelLeft = new Panel();
-		panelLeft.setLayout(new GridLayout(2,1));
-		btnExportLeft = new Button("Export to File, Left");
-		panelLeft.add(btnExportLeft);
-		
-		add(panelLeft, BorderLayout.WEST);
-		
-		//RIGHT
-		panelRight = new Panel();
-		panelRight.setLayout(new GridLayout(12,2,5,5));
-		btnExportRight = new Button("Export to File, Right");
-		panelRight.add(btnExportRight);
-		
-		add(panelRight, BorderLayout.EAST);
+
+		// CENTER
+		panelCenter = new Panel();
+		panelCenter.setLayout(new GridLayout(1, 2));
+		// panelCenter.add(panelLeft);
+		// panelCenter.add(panelRight);
+
+		add(panelCenter, BorderLayout.CENTER);
+
+		// // LEFT
+		// panelLeft = new Panel();
+		// panelLeft.setLayout(new GridLayout(2, 1));
+		// btnExportLeft = new Button("Export to File, Left");
+		// panelLeft.add(btnExportLeft);
+		// panelLeft.add(txtAreaLeft);
+		// // add(panelLeft);
+		//
+		// // RIGHT
+		// panelRight = new Panel();
+		// panelRight.setLayout(new GridLayout(2, 1));
+		// btnExportRight = new Button("Export to File, Right");
+		// panelRight.add(btnExportRight);
+		// panelRight.add(txtAreaRight);
+		// // add(panelRight);
+
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowActivated(WindowEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowClosed(WindowEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -94,28 +107,25 @@ public class MyFrame extends Frame implements WindowListener, ActionListener{
 	@Override
 	public void windowDeactivated(WindowEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowIconified(WindowEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowOpened(WindowEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
-
 
 }
